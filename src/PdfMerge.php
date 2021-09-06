611945001643a85ddd8c2aecd6718b4afb974a04
Borislav <cikabole@gmail.com>
2021-09-06 15:02:29 +0200

    Add ability to set tcpdf header and footer data (#1)
    * Add ability to set tcpdf header and footer data
    
    By passing feader and footer data in PdfMerge we can directly
    influence how and what data will be present while merging pdf
    files.
    
    The reason for implementing this feature in our case is that we want
    to get rid of lines in headers and footers, so we can set line color
    to white color.
    
    Co-authored-by: Borislav Jagodic <borislav.jagodic@karriere.at>

--- src/PdfMerge.php
+++ src/PdfMerge.php
@@ -12,6 +12,25 @@ class PdfMerge
      * @var array
      */
     private $files = [];
+    /**
+     * @var TCPDI
+     */
+    private $pdf;
+
+    /**
+     * Passed parameters overrides settings for header and footer by calling tcpdf.php methods:
+     * setHeaderData($ln='', $lw=0, $ht='', $hs='', $tc=array(0,0,0), $lc=array(0,0,0))
+     * setFooterData($tc=array(0,0,0), $lc=array(0,0,0))
+     * For more info about tcpdf, please read https://tcpdf.org/docs/
+     *
+     * @param array $headerConfig only values for keys 'ln', 'lw', 'ht', 'hs', 'tc', 'lc' are taken into account
+     * @param array $footerConfig only values for keys 'tc', 'lc' are taken into account
+     */
+    public function __construct(array $headerConfig = [], array $footerConfig = [])
+    {
+        $this->pdf = new TCPDI();
+        $this->configureHeaderAndFooter($headerConfig, $footerConfig);
+    }
 
     /**
      * Adds a file to merge
@@ -51,6 +70,7 @@ class PdfMerge
      * Generates a merged PDF file from the already stored pdf files
      * @param string $outputFilename the file to write to
      * @return bool
+     * @throws NoFilesDefinedException
      */
     public function merge(string $outputFilename): bool
     {
@@ -58,22 +78,39 @@ class PdfMerge
             throw new NoFilesDefinedException();
         }
 
-        $pdf = new TCPDI();
-
         foreach ($this->files as $file) {
-            $pageCount = $pdf->setSourceFile($file);
+            $pageCount = $this->pdf->setSourceFile($file);
 
             for ($i = 1; $i <= $pageCount; $i++) {
-                $pageId = $pdf->ImportPage($i);
-                $size = $pdf->getTemplateSize($pageId);
+                $pageId = $this->pdf->ImportPage($i);
+                $size = $this->pdf->getTemplateSize($pageId);
 
-                $pdf->AddPage('P', $size);
-                $pdf->useTemplate($pageId);
+                $this->pdf->AddPage('P', $size);
+                $this->pdf->useTemplate($pageId);
             }
         }
 
-        $pdf->Output($outputFilename, 'F');
+        $this->pdf->Output($outputFilename, 'F');
 
         return true;
+    }
+
+    private function configureHeaderAndFooter(array $headerConfig, array $footerConfig): void
+    {
+        if (count($headerConfig)) {
+            $ln = $headerConfig['ln'] ?? '';
+            $lw = $headerConfig['lw'] ?? 0;
+            $ht = $headerConfig['ht'] ?? '';
+            $hs = $headerConfig['hs'] ?? '';
+            $tc = $headerConfig['tc'] ?? [0, 0, 0];
+            $lc = $headerConfig['lc'] ?? [0, 0, 0];
+            $this->pdf->setHeaderData($ln, $lw, $ht, $hs, $tc, $lc);
+        }
+
+        if (count($footerConfig)) {
+            $tc = $footerConfig['tc'] ?? [0, 0, 0];
+            $lc = $footerConfig['lc'] ?? [0, 0, 0];
+            $this->pdf->setFooterData($tc, $lc);
+        }
     }
 }
