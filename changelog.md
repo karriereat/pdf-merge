7e6b60a0fb095d2e8f2a549fffe32be847b17600
Jakob Linskeseder <jakob.linskeseder@karriere.at>
2022-12-06 16:21:34 +0100

    Respect orientation when adding pages
    Also it makes no sense to display the default header-line
    when no header-data is passed.
    
    Closes #11, #12

--- changelog.md
+++ changelog.md
@@ -4,6 +4,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [2.1.0] - 2022-12-07
+### Added
+- Support for merging PDFs with mixed orientations (portrait and landscape)
+
+### Changed
+- Default header and footer are removed if header/footer-data is empty
+
 ## [2.0.0] - 2022-08-10
 ### Added
 - Support for PHP 8.1
--- tests/PdfMergeTest.php
+++ tests/PdfMergeTest.php
@@ -65,6 +65,37 @@ class PdfMergeTest extends TestCase
     }
 
     /** @test */
+    public function it_can_merge_portrait_and_landscape_files()
+    {
+        $pdfMerge = new PdfMerge();
+        $file = __DIR__ . '/files/dummy.pdf';
+        $fileHorizontal = __DIR__ . '/files/dummy_landscape.pdf';
+        $outputFile = __DIR__ . '/output.pdf';
+
+        $pdfMerge->add($file);
+        $pdfMerge->add($fileHorizontal);
+
+        $this->assertEquals('', $pdfMerge->merge($outputFile));
+        $this->assertPDFEquals(__DIR__ . '/files/expected/output_mixed_orientation.pdf', $outputFile);
+    }
+
+    /** @test */
+    public function it_adds_header_and_footer_to_the_merged_pdfs()
+    {
+        copy(__DIR__ . '/files/header_logo.jpg', K_PATH_IMAGES . 'header_logo.png');
+
+        $pdfMerge = new PdfMerge(['ln' => 'header_logo.png', 'lw' => 20, 'ht' => 'Test']);
+        $file = __DIR__ . '/files/dummy.pdf';
+        $outputFile = __DIR__ . '/output.pdf';
+
+        $pdfMerge->add($file);
+        $pdfMerge->add($file);
+
+        $this->assertEquals('', $pdfMerge->merge($outputFile));
+        $this->assertPDFEquals(__DIR__ . '/files/expected/output_with_header.pdf', $outputFile);
+    }
+
+    /** @test */
     public function it_fails_on_generate_when_no_files_were_added()
     {
         $this->expectException(NoFilesDefinedException::class);
